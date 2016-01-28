db194610483a240a13809ec2f8672f5a02684bad
lihy96 <lhy237451594@vip.qq.com>
2016-01-28 16:48:37 +0800

    First Commit

---
+++ src/win/lihy/game2048/Config.java
@@ -0,0 +1,5 @@
+package win.lihy.game2048;
+
+public class Config {
+	public static final int LINES = 4;
+}
---
+++ src/win/lihy/game2048/MainActivity.java
@@ -0,0 +1,56 @@
+package win.lihy.game2048;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+	private TextView tvScore;
+	private static MainActivity mainActivity = null;
+	private int score = 0;// 计分器
+
+	public MainActivity() {
+		mainActivity = this;
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		tvScore = (TextView) findViewById(R.id.tvScore);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public static MainActivity getMainActivity() {
+		return mainActivity;
+	}
+
+	public void clearScore() {
+		score = 0;
+		showScore();
+	}
+
+	public void showScore() {
+		tvScore.setText(score + "");
+	}
+
+	public void addScore(int s) {
+		score += s;
+		showScore();
+	}
+}
